4db9b2c78a0ad0c4eade00735eee4e90f3b197cf
Ayush Singhal <ayushsinghal304@gmail.com>
2025-09-29 19:47:47 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
             </Button>
             <Button variant="outline" size="lg" className="transition-bounce hover:scale-105 border-primary/50 hover:bg-primary/10 hover:shadow-stellar">
               <ExternalLink className="w-5 h-5 mr-2" />
-              <a href="https://drive.google.com/file/d/15s2ds8VW6Q6dKYNjBWwKCJ8RzFGoE6QM/view?usp=sharing">View Resume</a>
+              <a href="https://drive.google.com/file/d/16F6esFZ4SDm68iY3zXTkNdMyl-z62IO2/view?usp=drive_link">View Resume</a>
             </Button>
           </div>
 
